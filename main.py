a235971da725443d5b1aaf8ed7026fdc47b025d1
AbdelRahman Seliman <abdurrahmanmo07@hotmail.com>
2025-12-12 16:16:23 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+user_data = [ # Nested List,["Account Number","User PIN","Balance","Name"]
+    ["101","1111",2000,"Ahmed"],
+    ["102","2222",2000,"AbdelRahman"],
+    ["103","3333",2000,"Zeyad"],
+    ["104","4444",2000,"Moaz"],
+    ["105","5555",2000,"Mohamed"],
+]
+
+
+def login(): # take account number and pin as input from user
+
+
+def financial(): # Withdraw and Deposit
+
+def ShowBalance_ChangePIN(): # Show User Balance
+
+def admin(): # Add users and Manage theres pin and balance
+
+def main(): # Run Code all in one
